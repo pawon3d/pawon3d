0a9919b1a26c8d4531069c81d532141c71d6da0d
Muhammad Iqbal Firdaus <110145737+Kyudyoz@users.noreply.github.com>
2025-06-07 18:37:45 +0700

    sidebar dan navbar

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,9 +1,9 @@
 <div class="flex flex-row flex-nowrap items-center gap-2 px-6 py-4 whitespace-nowrap">
     <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-        <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+        class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-foreground text-accent-content">
+        <x-app-logo-icon class="size-5 fill-current text-accent-content" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-none font-semibold">Pawon3D</span>
+        <span class="mb-0.5 truncate leading-none font-semibold text-white">Pawon3D</span>
     </div>
 </div>
