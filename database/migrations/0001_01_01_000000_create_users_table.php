e23f0d21afad7c40f3785268f7c15a113b23c1de
Muhammad Iqbal Firdaus <110145737+Kyudyoz@users.noreply.github.com>
2025-12-26 13:55:37 +0700

    feat: Add initial user and session tables, and correct the password reset token schema.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -28,7 +28,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('id')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
